e6e28713d28e4aa3c55dcbba10200a548197a5e3
Muhammad Zaid <guyspakistan@gmail.com>
2024-10-09 20:19:57 +0500

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,6 @@
+a={5,6,33,74,3,7,22,8,3}
+b={77,22,6,10,31,5,6,88}
+print('Union:', a.union(b))
+print('Intersection:', a.intersection(b))
+print('Symmertic Difference', a.symmetric_difference(b))
+print('Difference:', a.difference(b))
